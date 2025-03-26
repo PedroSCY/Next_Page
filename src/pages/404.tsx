dcb2d2971d3a9a089c0ffbc1faecd208e5c5c95a
Pedro Lucas G. Prata <78231145+PedroSCY@users.noreply.github.com>
2025-03-26 14:06:48 -0300

    Rota 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import Pagina from "@/components/Pagina";
+import React from "react";
+
+export default function NaoEncontrado() {
+  return (
+    <Pagina>
+      <h1 className="text-8xl">404</h1>
+      <p className="text-base text-zinc-500">Ops... Página não encontrada!</p>
+    </Pagina>
+  );
+}
